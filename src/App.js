d00674a1c7467548555a353f406a6babd11988ab
Wissem Elayeb <elayebwissem24@gmail.com>
2023-05-09 18:14:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      person: {
+        fullName: 'John Doe',
+        bio: "I'm a web developer.",
+        imgSrc: 'https://example.com/image.jpg',
+        profession: 'Web Developer',
+      },
+      shows: false,
+      timeSinceMount: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        timeSinceMount: this.state.timeSinceMount + 1,
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const { person, shows, timeSinceMount } = this.state;
+
+    return (
+      <div>
+        <button onClick={() => this.setState({ shows: !shows })}>
+          {shows ? 'Hide' : 'Show'} Profile
+        </button>
+
+        {shows && (
+          <div>
+            <img src={person.imgSrc} alt={person.fullName} />
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <p>{person.profession}</p>
+          </div>
+        )}
+
+        <p>Time since mount: {timeSinceMount} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;

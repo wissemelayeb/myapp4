93dbbf75a78b0ec2a2d55651a0a3154576794853
Wissemelayeb <127353254+wissemelayeb@users.noreply.github.com>
2023-05-19 20:08:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+// App component
 class App extends Component {
   constructor(props) {
     super(props);
 
+    // Initialize state
     this.state = {
       person: {
         fullName: 'John Doe',
@@ -16,7 +18,9 @@ class App extends Component {
     };
   }
 
+  // Lifecycle method: componentDidMount
   componentDidMount() {
+    // Update timeSinceMount state every second
     this.interval = setInterval(() => {
       this.setState({
         timeSinceMount: this.state.timeSinceMount + 1,
@@ -24,28 +28,38 @@ class App extends Component {
     }, 1000);
   }
 
+  // Lifecycle method: componentWillUnmount
   componentWillUnmount() {
+    // Clear the interval when the component is unmounted
     clearInterval(this.interval);
   }
 
   render() {
+    // Destructure state values
     const { person, shows, timeSinceMount } = this.state;
 
     return (
       <div>
+        {/* Button to toggle the profile visibility */}
         <button onClick={() => this.setState({ shows: !shows })}>
           {shows ? 'Hide' : 'Show'} Profile
         </button>
 
+        {/* Conditional rendering of the profile */}
         {shows && (
           <div>
+            {/* Profile image */}
             <img src={person.imgSrc} alt={person.fullName} />
+            {/* Profile full name */}
             <h2>{person.fullName}</h2>
+            {/* Profile bio */}
             <p>{person.bio}</p>
+            {/* Profile profession */}
             <p>{person.profession}</p>
           </div>
         )}
 
+        {/* Display the time since mount */}
         <p>Time since mount: {timeSinceMount} seconds</p>
       </div>
     );
